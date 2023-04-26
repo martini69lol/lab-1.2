ede15b68da76e490279dec5a96a20ec58c4fd0db
maratdmitrichenki <maratdmitrichenki@gmail.com>
2023-04-26 14:41:51 +0300

    end def

--- program.py
+++ program.py
@@ -2,3 +2,5 @@ def b_sorter(array):
     length = len(array)
     for i in range(length):
         for j in range(0, length-i-1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
